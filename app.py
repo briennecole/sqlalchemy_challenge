b03afa4afaceedbebae2f3dc0b11e2c6d84e0eee
Brienne <cole.brienne@gmail.com>
2021-03-06 17:25:24 -0800

    flask app connection successful

---
+++ app.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+
+# Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+# Import Flask
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+# Home Page
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Welcome!<br/>"
+        f"Available Routes:<br/>"
+        f"<a href='/api/v1.0/precipitation'>precipitation</a><br/>"
+        f"<a href='/api/v1.0/stations'>stations</a><br/>"
+        f"<a href='/api/v1.0/tobs'>tobs</a><br/>"
+    )
+      
+# Precip page      
+#@app.route("/api/v1.0/precipitation")
+#def precipitation():
+
+if __name__ == '__main__':
+    app.run(debug=True)
